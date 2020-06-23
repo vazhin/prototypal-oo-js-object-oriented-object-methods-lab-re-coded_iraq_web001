7f4be87bf1c7ad3ce2b784f6feff0811968621a1
Vazhin Tayeb <vazhintayeb@gmail.com>
2020-06-23 04:47:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function BoardMember(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-
+  BoardMember.prototype.veto = function () {
+    return `No, I must disagree`
+  };
 }
